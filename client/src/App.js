e076b09a7ee8a11137156a0bc7801020c5166047
Your Name <ryibrahem1210@gmail.com>
2020-09-15 17:22:46 -0400

    footer and sidebar and last edits on the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
-
+import Video from "./components/Video";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>ER-NETWORK</h1>
+    <div className="app">
+      {/* APP CONTAINER */}
+      {/* VIDEOS */}
+      {/* VIDEOS */}
+      {/* VIDEOS */}
+      {/* VIDEOS */}
+      {/* VIDEOS */}
+      {/* VIDEOS */}
+
+      <div className="app__videos">
+        <Video
+          channel="user"
+          description="Diamonds "
+          song="Element - @Pop Smoke"
+          likes={3323}
+          messages={1234}
+          shares={221}
+        />
+        <Video
+          channel="user"
+          description="Diamonds "
+          song="Element - @Pop Smoke"
+          likes={3323}
+          messages={1234}
+          shares={221}
+        />
+      </div>
     </div>
   );
 }
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,11 +1,36 @@
-import React from "react";
-import "./Video";
-function Video() {
+import React, { useRef, useState } from "react";
+import "./Video.css";
+import vid from "./tiktok.mp4";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
+
+function Video({ url, channel, description, song, likes, messages, shares }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="video">
-      <video src="https://www.youtube.com/embed/HkuKHwetV6Q"></video>
-      {/* video footer */}
-      {/* videosidebar */}
+      <video
+        onClick={handleVideoPress}
+        className="video__player"
+        loop
+        ref={videoRef}
+        src={vid}
+        type="video/mp4"
+      ></video>
+
+      <VideoFooter channel={channel} description={description} song={song} />
+
+      <VideoSidebar likes={likes} shares={shares} messages={messages} />
     </div>
   );
 }
